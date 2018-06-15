3167616140329d15ce218cf98ae618a8874c0f3b
thephilgray <thephilgray@gmail.com>
2018-06-15 17:05:54 -0400

    Add the `products/:sku` route.

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Product from './views/Product.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -16,6 +17,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About
+    },
+    {
+      path: '/products/:sku',
+      name: 'product',
+      component: Product
     }
   ]
-})
+});
